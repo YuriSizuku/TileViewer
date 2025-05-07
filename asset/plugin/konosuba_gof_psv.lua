40b87377cef29fc7101c8711e31386b7d70c2bbd
devseed <devseed@163.com>
2025-05-07 16:44:11 +0900

    add konosuba gof font plugin'

---
+++ asset/plugin/konosuba_gof_psv.lua
@@ -0,0 +1,66 @@
+---@diagnostic disable : lowercase-global, missing-fields, undefined-global, duplicate-doc-field, undefined-field
+
+version = "v0.1"
+description = "[lua_konosuba_gof::init] lua plugin to decode Konosuba 4bpp non monospace font"
+
+-- global declear 
+g_data = nil --- @type string
+g_tilecfg = nil ---@type tilecfg_t
+g_fontmap = {}
+
+function decode_pre()
+    --- get neccessory data for decoding
+    g_tilecfg = get_tilecfg()
+    g_data = get_rawdata()
+
+    --- set the information for decoding
+    local bpp, w = string.unpack("<I1I1", g_data, 6+1)
+    local count = string.unpack("<I4", g_data, 0x20+1)
+    g_tilecfg.w, g_tilecfg.h, g_tilecfg.bpp = w, w, bpp
+    g_tilecfg.nbytes = 1
+    g_tilecfg.size = count
+    set_tilecfg(g_tilecfg)
+
+    log(string.format("[lua_baranoki_fnt::pre] datasize=%d w=%d h=%d bpp=%d nbytes=%d",
+        g_data:len(), g_tilecfg.w, g_tilecfg.h, g_tilecfg.bpp, g_tilecfg.nbytes))
+
+    -- set other intormations
+    offset_entry = string.unpack("<I4", g_data, 0x1c+1)
+    offset2, offset3, offset_glphy = string.unpack("<I4I4I4", g_data, 0x30+1)
+    for i=0,count do
+        local wchar, nbytes, tilew, tileh, x, y, padding, offset = string.unpack("<I2I2I1I1I1I1I4I4", g_data, offset_entry + i*24 + 1)
+        tilew = tilew + tilew %2
+        g_fontmap[i] = {wchar=wchar, nbytes=nbytes, tilew=tilew, tileh=tileh, offset=offset_glphy+offset}
+    end
+
+    return true
+end
+
+function decode_pixel(i, x, y)
+    if(g_fontmap[i]==nil) then return 0 end
+    if(g_fontmap[i].nbytes==0) then return 0 end
+    
+    local bpp = 4 -- supposed 4bpp
+    local w, h = g_fontmap[i].tilew, g_fontmap[i].tileh
+    if(x >= w or y >= h) then return 0 end
+    local pixeli = y*w + x
+    local offset = g_fontmap[i].offset + pixeli * bpp // 8
+    if(offset >= g_data:len()) then return 0 end
+
+    d = string.byte(g_data, offset + 1)
+    if(pixeli%2 == 0) then d = (d>>4) & 0xf
+    else d = d & 0xf end
+    d = 255 * d // (2<<bpp-1)
+
+    return d + (d << 8) + (d << 16) + (255 << 24)
+end
+
+function decode_post()
+    log("[lua_baranoki_fnt::post] decode finished")
+    set_tilenav({index=0, offset=-1})
+    set_tilestyle({scale=1, style=0})
+    g_data = nil
+    return true
+end
+
+log("   ", description, version)
